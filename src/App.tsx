e487dbf7fbd770e2d364ea0a29591e720ed65c39
Evgeny Kankin <evgenykankin@gmail.com>
2026-04-10 15:21:22 +0300

    Add Vitest

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.scss'
 
 function App() {
   return <h2>1.1.17 task Kankin E.
-              <br />Vite + React + TS</h2>
+              <br />Vite + React + TS + Vitest</h2>
 }
 
 export default App
